bf542c579f18d3d46edffce158982512fdb961c6
ijaamm <ijammusyaf@gmail.com>
2024-12-23 11:34:09 +0700

    membuat home dokter view model untuk mengelola data

---
+++ app/src/main/java/com/example/ucp2/viewmodel/HomeDrViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.ucp2.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Dokter
+import com.example.ucp2.repository.RepositoryDr
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeDrViewModel(
+    private val repositoryDr: RepositoryDr
+) : ViewModel() {
+
+    val homeDrUiState: StateFlow<HomeDrUiState> = repositoryDr.getAllDr()
+        .filterNotNull()
+        .map {
+            HomeDrUiState(
+                listDr = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeDrUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeDrUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeDrUiState(
+                isLoading = true
+            )
+        )
+
+}
+
+data class HomeDrUiState(
+    val listDr: List<Dokter> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
