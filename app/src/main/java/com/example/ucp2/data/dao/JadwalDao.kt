977da869988457d0cbc908bb649bcf6dd8afb473
ijaamm <ijammusyaf@gmail.com>
2024-12-23 21:17:12 +0700

    menambah get nama dokter untuk di panggil saat insert jadwal

--- app/src/main/java/com/example/ucp2/data/dao/JadwalDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/JadwalDao.kt
@@ -5,9 +5,9 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import com.example.ucp2.data.entity.Dokter
 import com.example.ucp2.data.entity.Jadwal
 import kotlinx.coroutines.flow.Flow
-
 
 @Dao
 interface JadwalDao {
@@ -20,6 +20,9 @@ interface JadwalDao {
 
     @Query("SELECT * FROM jadwal WHERE id = :id")
     fun getJadwal(id: String): Flow<Jadwal>
+
+    @Query("SELECT * FROM dokter ORDER BY nama DESC")
+    fun getAllNmDokter(): Flow<List<Dokter>>
 
     @Delete
     suspend fun deleteJadwal(jadwal: Jadwal)
