c9008116cd0940731409773c55a4dbd9685cf46b
ijaamm <ijammusyaf@gmail.com>
2024-12-23 11:57:43 +0700

    membuat home jadwal view

---
+++ app/src/main/java/com/example/ucp2/ui/view/jadwal/HomeJadwalView.kt
@@ -0,0 +1,230 @@
+package com.example.ucp2.ui.view.jadwal
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ucp2.data.entity.Jadwal
+import kotlinx.coroutines.launch
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.customwidget.TopAppBar
+import com.example.ucp2.viewmodel.HomeJadUiState
+import com.example.ucp2.viewmodel.HomeJadViewModel
+import com.example.ucp2.viewmodel.PenyediaViewModel
+
+
+@Composable
+fun CardJad (
+    Jad: Jadwal,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    Card (
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        onClick = onClick
+    ) {
+        Column (
+            modifier = Modifier.padding(8.dp)
+
+        ){
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(
+                    imageVector = Icons.Filled.Person,
+                    contentDescription = ""
+                )
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = Jad.NmPasien,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(
+                    imageVector = Icons.Filled.DateRange,
+                    contentDescription = ""
+                )
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = Jad.NmDokter,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(
+                    imageVector = Icons.Filled.Home,
+                    contentDescription = ""
+                )
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = Jad.tanggal,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ListJadwal(
+    listJad: List<Jadwal>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(
+            items = listJad,
+            itemContent = { jad ->
+                CardJad(
+                    Jad = jad,
+                    onClick = { onClick(jad.id) }
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun BodyHomeJadView(
+    HomeJadUiState: HomeJadUiState,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackBarHostState = remember { SnackbarHostState() }
+    when {
+        HomeJadUiState.isLoading -> {
+            // Menampilkan Indikator Loading
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        HomeJadUiState.isError -> {
+            // Menampilkan Snackbar jika terjadi kesalahan
+            LaunchedEffect(HomeJadUiState.errorMessage) {
+                HomeJadUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackBarHostState.showSnackbar(message) // Tampilkan Snackbar
+                    }
+                }
+            }
+        }
+
+        HomeJadUiState.listJad.isEmpty() -> {
+            // Menampilkan pesan jika daftar jadwal kosong
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak Ada Data Jadwal",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            // Menampilkan daftar jadwal jika tidak ada kesalahan
+            ListJadwal(
+                listJad = HomeJadUiState.listJad,
+                onClick = {
+                    onClick(it)
+                    println(it)
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun HomeJadView(
+    viewModel: HomeJadViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddJad: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    onBack: () -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    Scaffold (
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Jadwal",
+                showBackButton = true,
+                onBack = onBack,
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddJad,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Jadwal"
+                )
+            }
+        }
+    ) {
+            innerPadding ->
+        val HomeJadUiState by viewModel.homeJadUiState.collectAsState()
+
+        BodyHomeJadView(
+            HomeJadUiState = HomeJadUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
