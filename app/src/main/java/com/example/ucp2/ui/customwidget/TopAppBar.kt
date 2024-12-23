bcbcc34e5e87ed47b9ce3d9b15ed35673728e92d
ijaamm <ijammusyaf@gmail.com>
2024-12-23 21:20:20 +0700

    merubah tombol kembali menjadi icon arrowback

--- app/src/main/java/com/example/ucp2/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp2/ui/customwidget/TopAppBar.kt
@@ -6,12 +6,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,7 +30,6 @@ fun TopAppBar(
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.primary)
             .padding(15.dp),
         contentAlignment = Alignment.Center
     ){
@@ -34,11 +38,20 @@ fun TopAppBar(
                 modifier = Modifier.fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                TextButton(
+                IconButton(
                     onClick = onBack,
                     modifier = Modifier.align(Alignment.CenterVertically)
+                        .padding(10.dp)
+                        .background(
+                            color = Color.Black,
+                            shape = RoundedCornerShape(8.dp)
+                        )
                 ) {
-                    Text("Kembali")
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Kembali",
+                        tint = Color.White
+                    )
                 }
                 Spacer(modifier = Modifier.weight(2f))
             }
